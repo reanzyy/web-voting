bf89152da377521e610aa82a8f0339039e74c5d6
haikal <haikalsquat02@gmail.com>
2023-10-24 09:51:53 +0700

    making dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.dashboard');
+        // $selectedYear = $request->input('selected_year', Carbon::now()->format('Y'));
+
+        $query = DB::table('votes')
+            // ->whereYear('created_at', $selectedYear)
+            ->select('candidate_id', DB::raw('count(*) as count'))
+            ->groupBy('candidate_id')
+            ->get();
+
+        $chartData = [
+            'labels' => [],
+            'counts' => [],
+        ];
+
+        foreach ($query as $vote) {
+            $chartData['labels'][] = "Kandidat " . $vote->candidate_id;
+            $chartData['counts'][] = $vote->count;
+        }
+
+        return view('pages.dashboard', [
+            'chartData' => $chartData,
+        ]);
     }
 }
