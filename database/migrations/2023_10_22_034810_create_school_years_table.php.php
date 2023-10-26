4f030d5c8790b965f28556234c9582e897beecfa
Adriansyah Suryawan <suryawanadriansyah4@gmail.com>
2023-10-26 12:50:48 +0700

    table school years

---
+++ database/migrations/2023_10_22_034810_create_school_years_table.php.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school_years', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school_years');
+    }
+};
